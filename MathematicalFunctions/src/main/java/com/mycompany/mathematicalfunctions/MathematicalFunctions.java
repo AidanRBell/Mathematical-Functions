5231d431456c91222487e8505761f383441a3e5c
Aidan Bell <aidanrbell03@gmail.com>
2023-05-22 22:05:33 -0400

    Uploading Project to GitHub
    In this current state, only the 'Greatest Common Divisor' is set up

---
+++ MathematicalFunctions/src/main/java/com/mycompany/mathematicalfunctions/MathematicalFunctions.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mathematicalfunctions;
+
+/**
+ *
+ * @author aidan
+ */
+public class MathematicalFunctions {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+        
+        MainMenu menu = new MainMenu().getMainMenu();
+        menu.setVisible(true);
+        
+        System.out.println("end");
+
+    }
+}
